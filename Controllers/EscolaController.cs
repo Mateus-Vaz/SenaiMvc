b0004d06c9e3da438e6d51ff6a685b62db82b11f
MATEUS VAZ DOS SANTOS <mateus_v_santos@estudante.sesisenai.org.br>
2025-06-14 11:47:13 -0300

    Aula 02 mvc

---
+++ Controllers/EscolaController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SenaiMvc.Models.Escola;
+using SenaiMvc.Service.Interfaces;
+
+namespace SenaiMvc.Controllers
+{
+    public class EscolaController : Controller
+    {
+
+        private readonly IApiService _apiService;
+
+        public EscolaController(IApiService apiService)
+        {
+            _apiService = apiService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var escolas = await _apiService.GetAsync<List<EscolaModel>>("escola/buscar-todos");
+            return View(escolas);
+        }
+        [HttpGet]
+        public IActionResult Form()
+        {
+            var model = new EscolaModel();
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Form(EscolaModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var retorno = await _apiService.PostAsync<EscolaModel>("Escola", model);
+                return Redirect("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Editar(long id)
+        {
+            var model = await _apiService.GetAsync<EscolaModel>($"Escola/PegarPorId?id={id}");
+            return View("Form",model);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Excluir(long id)
+        {
+            var retorno = await _apiService.DeleteAsync<EscolaModel>($"Escola?id={id}");
+            return Redirect("/Escola/Index");
+        }
+    }
+}
---
+++ Models/Escola/EnderecoModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SenaiMvc.Models.Escola
+{
+    public class EnderecoModel
+    {
+    
+        public string Rua { get; set; }
+      
+        public string Bairro { get; set; }
+       
+        public string Cidade { get; set; }       
+
+        public string Estado { get; set; }
+
+        public int Numero { get; set; }
+
+    }
+}
